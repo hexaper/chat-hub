5261082a71d197a6e4bc5486456cee98e76d5f15
Your Name <you@example.com>
2026-03-10 22:41:03 +0100

    asgi

--- config/asgi.py
+++ config/asgi.py
@@ -1,10 +1,13 @@
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import apps.rooms.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
