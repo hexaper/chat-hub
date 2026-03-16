e1538ba182963b0a0188e69a2dee8de5d87b5cb8
hexaper <hexaper@gmail.com>
2026-03-16 16:50:14 -0400

    websocket origin

--- config/asgi.py
+++ config/asgi.py
@@ -3,7 +3,7 @@ import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 django.setup()
-
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
@@ -13,5 +13,11 @@ application = ProtocolTypeRouter({
     'http': get_asgi_application(),
     'websocket': AuthMiddlewareStack(
         URLRouter(apps.rooms.routing.websocket_urlpatterns)
+    ),
+})
+
+application = ProtocolTypeRouter({
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
     ),
 })
