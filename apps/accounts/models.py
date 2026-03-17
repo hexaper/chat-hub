463965fcb7f45cf8b745663e3fc677098450646d
hexaper <hexaper@gmail.com>
2026-03-16 20:49:38 -0400

    ssl in allinone

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class User(AbstractUser):
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
     bio = models.TextField(blank=True)
-    email = models.EmailField(unique=True, blank=True, null=True, default=None)
+    email = models.EmailField(unique=True, blank=True, null=True)
 
     def __str__(self):
         return self.username
