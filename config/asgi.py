46a1ceaca8b27d717190d35c3f77502db417da20
hexaper <hexaper@gmail.com>
2026-03-16 21:11:52 -0400

    static

--- config/asgi.py
+++ config/asgi.py
@@ -5,13 +5,12 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 django.setup()
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import apps.rooms.routing
 
 application = ProtocolTypeRouter({
-    'http': ASGIStaticFilesHandler(get_asgi_application()),
+    'http': get_asgi_application(),
     'websocket': AllowedHostsOriginValidator(
         AuthMiddlewareStack(URLRouter(apps.rooms.routing.websocket_urlpatterns))
     ),
